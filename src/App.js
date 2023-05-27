cb6c7ff09bf5eaf6d727eeaf4234ed4202781059
Hassan Ali <hassanali.bscsf19@iba-suk.edu.pk>
2023-05-27 16:54:08 +0500

    Added App  Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import PollDisplay from "./PollDisplay";
-import {data} from "./data";
 import UserParticipation from "./UserParticipation";
 import AnotherUserParticipation from "./AnotherUserParticipation";
-import {useState} from "react";
+import {Component, useState} from "react";
 import ThankYouMessage from "./ThankYouMessage";
 
-const App = () => {
 
-    const [disabledComponents, setDisabledComponents] = useState(false)
-    const handleClick = (value) => {
-        setDisabledComponents(value)
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            disabledComponents: false,
+            data: {
+                "question": "What is your favorite programming language?",
+                "choices": [
+                    {"id": 1, "label": "JavaScript", "votes": 0},
+                    {"id": 2, "label": "Python", "votes": 0},
+                    {"id": 3, "label": "Java", "votes": 0},
+                    {"id": 4, "label": "C#", "votes": 0}
+                ]
+
+            }
+        };
     }
 
-    return (
-        <div>
-            <ThankYouMessage disableComponents={disabledComponents}/>
-            <PollDisplay data={data}/>
-            <UserParticipation disableComponents={disabledComponents} handleClick={handleClick}/>
-            <AnotherUserParticipation handleClick={handleClick}/>
-        </div>
-    )
+    handleClick = (value) => {
+        this.setState({disabledComponents: value})
+    }
+    updateVote = (choice) => {
+        this.setState((prevState) => {
+            const data = prevState.data
+            const choiceObj = data.choices.find(d => d.label === choice)
+            choiceObj.votes++
+            return {data}
+        },() => {
+            console.log(this.state.data)
+        })
+    }
+
+    render() {
+        return (<div>
+            <ThankYouMessage disableComponents={
+                this.state.disabledComponents
+            }/>
+            <PollDisplay data={this.state.data}/>
+            <UserParticipation disableComponents={
+                this.state.disabledComponents
+            } handleClick={
+                this.handleClick
+            } updateVote={this.updateVote}
+                               choices={
+                                   this.state.data.choices
+                               }
+
+            />
+            <AnotherUserParticipation handleClick={
+                this.handleClick
+            }/>
+        </div>)
+    }
+
 }
 
 export default App
--- src/UserParticipation.js
+++ src/UserParticipation.js
@@ -1,43 +1,25 @@
-import {Form} from "react-router-dom";
-import {data, users} from "./data";
-import {useState} from "react";
-
 const UserParticipation = (props) => {
 
     const submitHandle = (e) => {
         e.preventDefault()
-
-
-        // get data from form
         const formData = new FormData(e.target)
-        const name = formData.get("name")
         const choice = formData.get("choice")
-        console.log(name, choice)
+        console.log(choice)
 
-        // check if user has already voted
-        if (users[name]) {
-            alert("You have already voted")
-            return
-        }
-        users[name] = choice
+
         props.handleClick(true)
         // vote update in data
-        data.choices.find(d => d.label === choice).votes++
+        props.updateVote(choice)
 
         e.target.reset()
-
-        console.log(users)
-        console.log(data)
 
     }
 
 
     return <form onSubmit={submitHandle}>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" disabled={props.disableComponents}/>
         <label htmlFor="choice">Choice</label>
         <select id="choice" name="choice" disabled={props.disableComponents}>
-            {data.choices.map(d => <option key={d.id} value={d.label}>{d.label}</option>)}
+            {props.choices.map(d => <option key={d.id} value={d.label}>{d.label}</option>)}
         </select>
         <button type="submit">Submit</button>
     </form>
